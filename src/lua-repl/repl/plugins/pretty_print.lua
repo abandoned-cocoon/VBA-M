7ee1e8cd73ba3b6454423f3bdaac043401afec6c
Example <example@example.com>
2014-08-01 17:27:27 +0200

    update lua_repl to commit b5fde2ac342def303eda8eb8d51b2bc807fdc4ba

--- src/lua-repl/repl/plugins/pretty_print.lua
+++ src/lua-repl/repl/plugins/pretty_print.lua
@@ -1,4 +1,4 @@
--- Copyright (c) 2011-2012 Rob Hoelz <rob@hoelz.ro>
+-- Copyright (c) 2011-2013 Rob Hoelz <rob@hoelz.ro>
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of
 -- this software and associated documentation files (the "Software"), to deal in
@@ -19,6 +19,7 @@
 -- Pretty prints expression results (console only)
 
 local format   = string.format
+local tconcat  = table.concat
 local tsort    = table.sort
 local tostring = tostring
 local type     = type
@@ -81,6 +82,7 @@ if term then
     boolean     = term.colors.green,
     number      = term.colors.cyan,
     path        = term.colors.white,
+    misc        = term.colors.magenta,
   }
 end
 
@@ -92,13 +94,43 @@ local function isident(s)
   return type(s) == 'string' and not keywords[s] and s:match('^[a-zA-Z_][a-zA-Z0-9_]*$')
 end
 
+-- most of these are arbitrary, I *do* want numbers first, though
+local type_order = {
+  number       = 0,
+  string       = 1,
+  userdata     = 2,
+  table        = 3,
+  thread       = 4,
+  boolean      = 5,
+  ['function'] = 6,
+}
+
+local function cross_type_order(a, b)
+  local pos_a = type_order[ type(a) ]
+  local pos_b = type_order[ type(b) ]
+
+  if pos_a == pos_b then
+    return a < b
+  else
+    return pos_a < pos_b
+  end
+end
+
 local function sortedpairs(t)
   local keys = {}
 
+  local seen_non_string
+
   for k in pairs(t) do
     keys[#keys + 1] = k
+
+    if not seen_non_string and type(k) ~= 'string' then
+      seen_non_string = true
+    end
   end
-  tsort(keys)
+
+  local sort_func = seen_non_string and cross_type_order or nil
+  tsort(keys, sort_func)
 
   local index = 1
   return function()
@@ -124,21 +156,27 @@ local function find_longstring_nest_level(s)
   return level
 end
 
-local function dump(seen, path, v, indent)
+local function dump(params)
+  local pieces = params.pieces
+  local seen   = params.seen
+  local path   = params.path
+  local v      = params.value
+  local indent = params.indent
+
   local t = type(v)
 
   if t == 'nil' or t == 'boolean' or t == 'number' then
-    stderr:write(colormap[t](tostring(v)))
+    pieces[#pieces + 1] = colormap[t](tostring(v))
   elseif t == 'string' then
     if v:match '\n' then
       local level = find_longstring_nest_level(v)
-      stderr:write(colormap.string('[' .. string.rep('=', level) .. '[' .. v .. ']' .. string.rep('=', level) .. ']'))
+      pieces[#pieces + 1] = colormap.string('[' .. string.rep('=', level) .. '[' .. v .. ']' .. string.rep('=', level) .. ']')
     else
-      stderr:write(colormap.string(format('%q', v)))
+      pieces[#pieces + 1] = colormap.string(format('%q', v))
     end
   elseif t == 'table' then
     if seen[v] then
-      stderr:write(colormap.path(seen[v]))
+      pieces[#pieces + 1] = colormap.path(seen[v])
       return
     end
 
@@ -146,50 +184,78 @@ local function dump(seen, path, v, indent)
 
     local lastintkey = 0
 
-    stderr:write(colormap.punctuation '{\n')
+    pieces[#pieces + 1] = colormap.punctuation '{\n'
     for i, v in ipairs(v) do
       for j = 1, indent do
-        stderr:write '  '
+        pieces[#pieces + 1] = '  '
       end
-      dump(seen, path .. '[' .. tostring(i) .. ']', v, indent + 1)
-      stderr:write(colormap.punctuation ',\n')
+      dump {
+        pieces = pieces,
+        seen   = seen,
+        path   = path .. '[' .. tostring(i) .. ']',
+        value  = v,
+        indent = indent + 1,
+      }
+      pieces[#pieces + 1] = colormap.punctuation ',\n'
       lastintkey = i
     end
 
     for k, v in sortedpairs(v) do
       if not (isinteger(k) and k <= lastintkey and k > 0) then
         for j = 1, indent do
-          stderr:write '  '
+          pieces[#pieces + 1] = '  '
         end
 
         if isident(k) then
-          stderr:write(colormap.ident(k))
+          pieces[#pieces + 1] = colormap.ident(k)
         else
-          stderr:write(colormap.punctuation '[')
-          dump(seen, path .. '.' .. tostring(k), k, indent + 1)
-          stderr:write(colormap.punctuation ']')
+          pieces[#pieces + 1] = colormap.punctuation '['
+          dump {
+            pieces = pieces,
+            seen   = seen,
+            path   = path .. '.' .. tostring(k),
+            value  = k,
+            indent = indent + 1,
+          }
+          pieces[#pieces + 1] = colormap.punctuation ']'
         end
-        stderr:write(colormap.punctuation ' = ')
-        dump(seen, path .. '.' .. tostring(k), v, indent + 1)
-        stderr:write(colormap.punctuation ',\n')
+        pieces[#pieces + 1] = colormap.punctuation ' = '
+        dump {
+          pieces = pieces,
+          seen   = seen,
+          path   = path .. '.' .. tostring(k),
+          value  = v,
+          indent = indent + 1,
+        }
+        pieces[#pieces + 1] = colormap.punctuation ',\n'
       end
     end
 
     for j = 1, indent - 1 do
-      stderr:write '  '
+      pieces[#pieces + 1] = '  '
     end
 
-    stderr:write(colormap.punctuation '}')
+    pieces[#pieces + 1] = colormap.punctuation '}'
   else
-    error(format('Cannot print type \'%s\'', t))
+    pieces[#pieces + 1] = colormap.misc(tostring(v))
   end
 end
 
 repl:requirefeature 'console'
 
 function override:displayresults(results)
+  local pieces = {}
+
   for i = 1, results.n do
-    dump({}, '<topvalue>', results[i], 1)
-    stderr:write '\n'
+    dump {
+      pieces = pieces,
+      seen   = {},
+      path   = '<topvalue>',
+      value  = results[i],
+      indent = 1,
+    }
+    pieces[#pieces + 1] = '\n'
   end
+
+  stderr:write(tconcat(pieces, ''))
 end
